7a3eb0162f9bb0885c6d74a71bc6c7c250b8480d
Andy Ting Yu Kuo <akuo34@Andys-MacBook-Air.local>
2020-08-12 02:12:37 -0700

    set up front end router/pages

--- src/App.js
+++ src/App.js
@@ -8,32 +8,35 @@ import {
 
 const Home = React.lazy(() => import('./Home'));
 const About = React.lazy(() => import('./About'));
+const Products = React.lazy(() => import('./Products'));
+const Contact = React.lazy(() => import('./Contact'));
 const Admin = React.lazy(() => import('./Admin'));
-// const Events = React.lazy(() => import('./Events'));
 
 const App = () => {
   return (
     <div className="App">
-      <div className="toolbar-main">
-        <React.Suspense fallback={<span></span>}>
-          <Router>
-            <ul>
-              <li>
-                <Link className="link" to="/">Home</Link>
-              </li>
-              <li>
-                <Link className="link" to="/about">About</Link>
-              </li>
-            </ul>
-            <Switch>
-              <Route path="/about" render={() => <About />} />
-              <Route path="/admin" render={() => <Admin />} />
-              {/* <Route path="/products" render={() => <Products />} /> */}
-              <Route exact path="/" render={() => <Home />} />
-            </Switch>
-          </Router>
-        </React.Suspense>
-      </div>
+      <React.Suspense fallback={<span></span>}>
+        <Router>
+          <div className="toolbar-main">
+            <div className="header-company">
+              <Link className="link" to="/">United Medicare</Link>
+            </div>
+            <div className="container-links">
+              <Link className="link" to="/">Home</Link>
+              <Link className="link" to="/about">About</Link>
+              <Link className="link" to="/products">Products</Link>
+              <Link className="link" to="/contact">Contact</Link>
+            </div>
+          </div>
+          <Switch>
+            <Route path="/admin" render={() => <Admin />} />
+            <Route path="/contact" render={() => <Contact />} />
+            <Route path="/products" render={() => <Products />} />
+            <Route path="/about" render={() => <About />} />
+            <Route exact path="/" render={() => <Home />} />
+          </Switch>
+        </Router>
+      </React.Suspense>
     </div>
   );
 }
